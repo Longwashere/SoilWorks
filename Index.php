ec2df5660d32340ccfc78e49df6418a71d323e8f
Longphihoang <Long.p.hoang1@gmail.com>
2017-03-17 02:59:15 -0700

    Initial commit

---
+++ Index.php
@@ -0,0 +1,74 @@
+<html>
+
+<title>
+    Soilworks
+</title>
+
+<head>
+    <link rel="stylesheet" href="CSS/style.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+<div class="container-fluid">
+    <div class="row">
+        <ul class="nav nav-tabs">
+            <li class="dropdown pull-right">
+                <a href="" class="dropdown-toggle" data-toggle="dropdown" href="#">
+                    Soil-X
+                </a>
+            </li>
+            <li class="dropdown pull-right">
+                <a class="dropdown-toggle " data-toggle="dropdown" href="#">
+                    Soil Media
+                </a>
+                <ul class="dropdown-menu " role="menu">
+                    <li><a href="#">Soil Profile</a></li>
+                    <li><a href="#">Videos</a></li>
+                    <li><a href="#">Photos</a></li>
+                    <li><a href="#">Infographics</a></li>
+
+                </ul>
+            </li>
+            <li class="dropdown pull-right">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                    Soil Matters
+                </a>
+                <ul class="dropdown-menu" role="menu">
+                    <li><a href="">Web Of Soil</a></li>
+                    <li><a href="">Articles/News</a></li>
+                    <li><a href="">Educational</a></li>
+                    <li><a href="">Soil Facts</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</div>
+
+<div class="imageOverFont">
+    <img class="img-responsive" style="" src="Images/HeaderPhoto1.jpg">
+    <h2 class="fontOverImage">woehqewqygewyqgey</h2>
+
+</div>
+
+
+</body>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: long
+ * Date: 3/16/2017
+ * Time: 7:51 PM
+ */
+?>
+
+</html>
