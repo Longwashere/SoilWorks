c809952de113443d2b0071f9f3882adccbfe2681
Longphihoang <Long.p.hoang1@gmail.com>
2017-03-29 15:58:24 -0700

    Finished Top Side With Headers

--- Index.php
+++ Index.php
@@ -6,6 +6,7 @@
 
 <head>
     <link rel="stylesheet" href="CSS/style.css">
+    <link rel="stylesheet" href="CSS/animation.css">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Latest compiled and minified CSS -->
@@ -57,18 +58,94 @@
 
 
 <div style="padding-left: 0px; padding-right: 0px" class="container-fluid ">
-    <img class="img-responsive" src="Images/HeaderPhoto1.jpg">
-    <h2 class="fontOverImage">WE ALL SHARE<br>COMMON GROUND.<br> </h2>
+    <img  src="Images/HeaderPhoto1.jpg" style="width:100%; height: 90%">
+    <h2 class="fontOverImage">WE ALL SHARE<br>COMMON GROUND.<br></h2>
     <h2 class="fontOverImage2"><span>get dirty. join the revolution</span></h2>
 
 </div>
 
 <img class="imageFit" src="Images/SoilWorkLogo.png">
 
+<div class="container-fluid" style="padding-top: 2vh">
+
+    <div class="row">
+        <div class="col-md-1 col-centered">
+            <img class="img-responsive" alt="">
+        </div>
+
+        <div class="col-md-1 col-centered">
+            <img class="img-responsive" src="http://placehold.it/200x200/000000/ffffff" alt="" Style="animation-name: transparent;
+    animation-duration: 10s;">
+
+        </div>
+
+        <div class="col-md-1 col-centered">
+            <img class="img-responsive" style="animation-name: transparent;
+    animation-duration: 10s;" src="" alt="arrow">
+
+        </div>
+
+        <div class="col-md-1 col-centered">
+            <img class="img-responsive" src="http://placehold.it/200x200/000000/ffffff" alt="" Style="animation-name: transparent;
+    animation-duration: 10s;">
+
+        </div>
+
+        <div class="col-md-1 col-centered">
+            <img class="img-responsive" Style="animation-name: transparent;
+    animation-duration: 10s;" src="" alt="arrow">
+
+        </div>
+
+        <div class="col-md-1 col-centered">
+            <img class="img-responsive" src="http://placehold.it/200x200/000000/ffffff" Style="animation-name: transparent;
+    animation-duration: 10s;" alt="">
+
+        </div>
+
+
+        <div class="col-md-1 col-centered">
+            <img class="img-responsive" src="" alt="arrow" Style="animation-name: transparent;
+    animation-duration: 10s;">
+        </div>
+
+        <div class="col-md-1 col-centered">
+            <img class="img-responsive" src="http://placehold.it/200x200/000000/ffffff" alt="" Style="animation-name: transparent;
+    animation-duration: 10s;">
+
+        </div>
+
+
+        <div class="col-md-1 col-centered">
+            <img class="img-responsive" src="" alt="arrow" Style="animation-name: transparent;
+    animation-duration: 10s;">
+        </div>
+
+
+        <div class="col-md-1 col-centered">
+            <img class="img-responsive" src="http://placehold.it/200x200/000000/ffffff" alt="arrow" Style="animation-name: transparent;
+    animation-duration: 10s;">
+
+        </div>
+
+
+        <div class="col-md-1 col-centered">
+            <img class="img-responsive" src="" alt="">
+
+        </div>
+
+
+        <div class="col-md-1 col-centered">
+            <img class="img-responsive" alt="">
+        </div>
+
+    </div>
+
+</div>
 
 
 <div class="container-fluid">
-    <p  class="text-center" style="font-size: 3em; padding-top:20vh;">
+    <p class="text-center" style="font-size: 3em; padding-top:20vh;">
         We believe that healthy soils <br> makes for a healthy planet.
     </p>
 </div>
@@ -76,25 +153,25 @@
 
 <div style="font-size: 2em" class=" text-center container-fluid">
     <p style="padding-top:20vh; display: inline-block; text-align: left">
-      <strong>  For this generation of <a href=""> eaters</a> <br>
-        For this generation of <a href=""> activists</a> <br>
-        For this generation of <a href=""> researchers</a> <br>
-        For this generation of <a href=""> teachers</a> <br>
-        For this generation of <a href=""> growers</a> <br>
-        For this generation of <a href=""> doers</a> <br>
-      </strong>
+        <strong> For this generation of <a href=""> eaters</a> <br>
+            For this generation of <a href=""> activists</a> <br>
+            For this generation of <a href=""> researchers</a> <br>
+            For this generation of <a href=""> teachers</a> <br>
+            For this generation of <a href=""> growers</a> <br>
+            For this generation of <a href=""> doers</a> <br>
+        </strong>
     </p>
 </div>
 
 
-
-<h2 style="padding-top: 20vh; padding-bottom: 10px"><span style="font-size: 1em;padding-left: 2%; padding-right: 25%">soil profiles</span></h2>
+<h2 style="padding-top: 20vh; padding-bottom: 10px"><span style="font-size: 1em;padding-left: 2%; padding-right: 25%">soil profiles</span>
+</h2>
 
 <div class="container">
     <!-- Projects Row -->
-    <div  class="row">
-        <div  class="col-md-4 portfolio-item">
-            <a  href="#">
+    <div class="row">
+        <div class="col-md-4 portfolio-item">
+            <a href="#">
                 <img class="img-responsive" src="http://placehold.it/750x750/000000/ffffff" alt="">
             </a>
         </div>
@@ -125,8 +202,13 @@
         </div>
 
     </div>
-    <!-- /.row -->
+</div>
+<!-- /.row -->
 
+<h2 style="padding-top: 0vh; padding-bottom: 10px"><span style="font-size: 1em;padding-left: 2%; padding-right: 25%">The Story of Soil</span>
+</h2>
+
+Video or Soil graph here?
 </body>
 
 
